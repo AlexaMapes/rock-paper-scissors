03ddd921d0f5e98700cba786785de9b1a01328ea
Lexi <alexamapes13@gmail.com>
2022-08-05 22:18:35 -0400

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -10,20 +10,43 @@ function getComputerChoice() {
     switch (computerChoice) {
         case 0:
             console.log("Rock");
-            return("rock");
+            return computerChoice;
             break;
         case 1:
             console.log("Paper");
-            return("paper");
+            return computerChoice;
             break;
         case 2:
             console.log("Scissors")
-            return
+            return computerChoice;
             break;
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
+    let playerSelectionInt = 0;
 
+    if (playerSelection == "rock") {
+        playerSelectionInt = 0;
+    }
+    else if (playerSelection == "paper") {
+        playerSelectionInt = 1;
+    }
+    else {
+        playerSelectionInt = 2;
+    }
+
+    if (((playerSelectionInt == 0) && (computerSelection == 0)) || ((playerSelectionInt == 1) && (computerSelection == 1)) || ((playerSelectionInt == 2) && (computerSelection == 2))) {
+        console.log("It's a tie!")
+        return 0;
+    }
+    else if (((playerSelectionInt == 0) && (computerSelection == 2)) || ((playerSelectionInt == 1) && (computerSelection == 0)) || ((playerSelectionInt == 2) && (computerSelection == 1))) {
+        console.log("Player wins!")
+        return 1;
+    }
+    else {
+        console.log("Computer wins!")
+        return 2;
+    }
 }
