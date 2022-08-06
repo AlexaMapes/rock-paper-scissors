9a3dad475345955324661eec6af3811cd2559e74
Lexi <alexamapes13@gmail.com>
2022-08-05 22:27:36 -0400

    Create game function

--- javascript.js
+++ javascript.js
@@ -6,18 +6,17 @@ function getRandomInt(min, max) {
 
 function getComputerChoice() {
     let computerChoice = getRandomInt(0, 3);
-    console.log(computerChoice);
     switch (computerChoice) {
         case 0:
-            console.log("Rock");
+            console.log("Computer chose rock");
             return computerChoice;
             break;
         case 1:
-            console.log("Paper");
+            console.log("Computer chose paper");
             return computerChoice;
             break;
         case 2:
-            console.log("Scissors")
+            console.log("Computer chose scissors")
             return computerChoice;
             break;
     }
@@ -28,13 +27,16 @@ function playRound(playerSelection, computerSelection) {
     let playerSelectionInt = 0;
 
     if (playerSelection == "rock") {
+        console.log("You chose rock")
         playerSelectionInt = 0;
     }
     else if (playerSelection == "paper") {
+        console.log("You chose paper")
         playerSelectionInt = 1;
     }
     else {
         playerSelectionInt = 2;
+        console.log("You chose scissors")
     }
 
     if (((playerSelectionInt == 0) && (computerSelection == 0)) || ((playerSelectionInt == 1) && (computerSelection == 1)) || ((playerSelectionInt == 2) && (computerSelection == 2))) {
@@ -50,3 +52,38 @@ function playRound(playerSelection, computerSelection) {
         return 2;
     }
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let roundResult = 0;
+
+    for (let i = 0; i < 5; i++) {
+        console.log("Round " + (i + 1));
+        roundResult = playRound(prompt("Choose rock, paper, or scissors"), getComputerChoice());
+        
+        if (roundResult == 0) {
+            console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
+        }
+        else if (roundResult == 1) {
+            playerScore++;
+            console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
+        }
+        else {
+            computerScore++
+            console.log("Your score: " + playerScore + " | Computer score: " + computerScore);
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You win!");
+    }
+    else if (computerScore > playerScore) {
+        console.log("Computer wins!");
+    }
+    else {
+        console.log("It's a tie!");
+    }
+}
+
+game();
